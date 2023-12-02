2800c36541b1dbf7d48cadbad7bd4900c9588a15
anyaaa213 <152586900+anyaaa213@users.noreply.github.com>
2023-12-02 07:15:30 -0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$username = "usernini";
+$password = "12345678";
+$database = "usernini";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+$ID_Number = $_POST['ID_Number'];
+$LLCCemail = $_POST['LLCCemail'];
+$Nametag = $_POST['Nametag'];
+$Password = password_hash($_POST['Password'], PASSWORD_DEFAULT); // Hash the password for security
+
+// Use prepared statement to prevent SQL injection
+$stmt = $conn->prepare("INSERT INTO signup (ID_Number, LLCCemail, Nametag, Password) VALUES (?, ?, ?, ?)");
+
+if ($stmt === false) {
+    die("Error preparing statement: " . $conn->error);
+}
+
+$stmt->bind_param("isss", $ID_Number, $LLCCemail, $Nametag, $Password);
+$stmt->execute();
+
+if ($stmt->affected_rows > 0) {
+    echo "Sign up successful...";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+// Close statement and connection
+$stmt->close();
+$conn->close();
+?>
